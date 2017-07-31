2a61c6bd764a6782e4bc81630e43a31aae1dafcd
preeti <piolAtif@users.noreply.github.com>
2017-07-31 16:40:30 +0530

    Added onclick and event for knowing current clicked link

--- src/App.js
+++ src/App.js
@@ -13,17 +13,38 @@ import User from "./User";
 
 class Routers extends React.Component {
     constructor(){
-        super()
+        super();
+        this.state = {selectedItem:'home'};
     }
+
+    onItemClick(event){
+        this.setState({selectedItem:event.target.id})
+    }
+
+    componentWillMount(){
+
+    }
+
+
     render() {
         return (
             <Router>
                 <div>
                     <div className="menu">
                         <ul className="nav_main">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/allBooks">All books</Link></li>
-                            <li><Link to="/user">User</Link></li>
+
+                            <li><Link to="/" id="home"
+                                      className={this.state.selectedItem === 'home' ? "active": "inactive"}
+                                      onClick={this.onItemClick.bind(this)}>Home</Link>
+                            </li>
+                            <li><Link to="/allBooks" id="all_books"
+                                      className={this.state.selectedItem === 'all_books' ? "active": "inactive"}
+                                      onClick={this.onItemClick.bind(this)}>All Books</Link>
+                            </li>
+                            <li><Link to="/user" id="user"
+                                      className={this.state.selectedItem === 'user' ? "active": "inactive"}
+                                      onClick={this.onItemClick.bind(this)}>User</Link>
+                            </li>
                         </ul>
                     </div>
 
