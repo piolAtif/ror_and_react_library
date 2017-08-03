7bb70676122611dc74d40eda2785725d43e29d21
preeti <piolAtif@users.noreply.github.com>
2017-08-03 14:05:05 +0530

    Added cors proxy with urls

--- src/service/httpService.js
+++ src/service/httpService.js
@@ -1,10 +1,11 @@
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 class httpService{
     constructor(url){
         this.url = url;
     }
 
     getBooks(callback){
-        let url = this.url;
+        let url = PROXY_URL+this.url;
         fetch(`${url}`)
             .then(response=>{
                 return response.json()
